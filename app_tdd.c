f76bc62d4461638acd643f52127b98b8319e3c82
Darrin M. Gorski <darrin@gorski.net>
2022-09-15 18:25:43 -0400

    Update app_tdd.c
    backward ifdef condition for spandsp_log variation.

--- app_tdd.c
+++ app_tdd.c
@@ -290,7 +290,7 @@ AST_APP_OPTIONS(starttddrx_opts, {
  *
  * \note This is a spandsp callback function
  */
-#if SPANDSP_RELEASE_DATE < 20120902
+#if SPANDSP_RELEASE_DATE >= 20120902
 static void spandsp_log(NULL, int level, const char *msg)
 #else
 static void spandsp_log(int level, const char *msg)
